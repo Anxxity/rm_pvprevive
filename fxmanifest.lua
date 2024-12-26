4f212a807e8c3cf93a20320dbf4d417b9ce1bf37
anxxity <reminmohammed123@gmail.com>
2024-12-27 00:30:50 +0530

    fxmanifest update

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game "gta5"
+
+author "Remin Moahmmed"
+description 'A Bot revive System for pvp'
+lua54 'yes'
+
+
+
+client_script {
+    'client.lua'
+}
+
+server_script {
+    "server.lua",
+}
