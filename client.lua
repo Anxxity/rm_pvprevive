da0814f1753f1f12da1cc765830627a31e68835f
anxxity <reminmohammed123@gmail.com>
2024-12-27 00:36:51 +0530

    dead check

--- client.lua
+++ client.lua
@@ -1,3 +1,25 @@
+local isDead = false
+-- checking if player is dead and showing TextUI
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(500) 
+        local playerPed = PlayerPedId()
+        if IsPedDeadOrDying(playerPed, true) then --checking if dead
+            if not isDead then  
+                isDead = true   
+                Citizen.Wait(2000)  --waiting 2 sec
+                if IsPedDeadOrDying(playerPed, true) then
+                     --calling function to shown text ui
+                end
+            end
+        elseif isDead then
+            isDead = false
+             --calling function to hide text ui
+        end
+        
+    end
+end)
+
 --anti wipping added for pvp 
 Citizen.CreateThread(function()
     while true do
